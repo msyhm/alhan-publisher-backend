6f94174c05897d9109f0923d47413a06b2538b16
msyhm <masih12w@gmail.com>
2026-07-05 09:49:25 +0330

    اصلاح مشکل لودینگ تصاویر

--- src/server.js
+++ src/server.js
@@ -17,6 +17,7 @@ import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 dotenv.config();
 
 const app = express();
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 4000;
 
 // ─── میدلورهای امنیتی و پایه ──────────────────────────────────────────────
